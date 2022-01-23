b39082cdaa80f5314db3241a3a42b6572244f225
adrian delgado zambrano <jodezamb@espol.edu.ec>
2022-01-23 18:10:35 -0500

    ejemplos de clouseres

---
+++ closures/closures.py
@@ -0,0 +1,28 @@
+
+
+# son un tipo de caso especial con 3 reglas 
+#1. Tenemos una nesf funcion  
+#2.la nest funcion referencia a un valor  de un scoope superior 
+#3 este nest funcion es retornada
+
+
+
+#progrma hola 3 -> HOLAHOLAHOLA
+
+
+
+def make_repeater_of(n):
+    def repeater(string):
+        assert type(string)==str,'Solo se puede usar cadenda de caracteres'
+        return string*n
+    return repeater
+
+def run():
+    repeta_5=make_repeater_of(5)
+    print (repeta_5('Bienvenidos'))
+
+
+if __name__ == '__main__':
+    run()
+
+
