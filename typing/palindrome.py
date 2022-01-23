fde408258e74040796b8db9858940b34691ea69e
adrian delgado zambrano <jodezamb@espol.edu.ec>
2022-01-23 15:47:45 -0500

    revisando errores con mypy palindrome.py --check-untyped-defs

--- typing/palindrome.py
+++ typing/palindrome.py
@@ -10,9 +10,9 @@ def is_palindrome(string: str)-> bool:
     return string==string[::-1]  # se retorna el valor boleano  de comparar el string al revez. 
 
 
-
+# para revisar si existe un error se ejecuta ,  << mypy palindrome.py --check-untyped-defs  >> en la consola 
 def run():
-    print(is_palindrome(1000)) #error  ya que la funcion espera  recibir un string no un int
+    print(is_palindrome('ana')) #error  ya que la funcion espera  recibir un string no un int
 
 
 if __name__ == '__main__':
