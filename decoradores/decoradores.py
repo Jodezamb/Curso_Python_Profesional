a68947b871d54280cef0146918d6779e9a26f344
adrian delgado zambrano <jodezamb@espol.edu.ec>
2022-01-23 21:03:09 -0500

    Concepto de decoradores  y sugar syntax

---
+++ decoradores/decoradores.py
@@ -0,0 +1,18 @@
+# un decorador es una funcion que recibe como parameto otra funcion, le anade cosas, y retorna una funcion diferente. un decoraodor tambien es un clousere especial 
+
+from tkinter import W
+from importlib_metadata import re
+
+
+def mayasculas(func):
+    def wrapper(texto):
+        return func(texto).upper()
+    return wrapper
+
+
+@mayasculas
+def mensaje(nombre):
+    return f'{nombre}, recibistes un mensaje'
+
+print(mensaje("Adrian"))
+
