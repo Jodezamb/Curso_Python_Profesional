b5267c027494800c850309dca2808c8ed65310ff
adrian delgado zambrano <jodezamb@espol.edu.ec>
2022-01-23 22:19:19 -0500

    modificando la funcion execution_time para que funcione con cualquier tipo de funcion

--- decoradores/decorators.py
+++ decoradores/decorators.py
@@ -4,9 +4,9 @@ from datetime import datetime
 # analizar eficiencia
 
 def execution_time(func):
-    def wrapper():
-        initial_time=datetime.now()
-        func()
+    def wrapper(*args,**kwargs): # estos parametros son para que pueda funcionar cualquier funcion, no importa l cantida de argumentos posicionales que se le envie 
+        initial_time=datetime.now() # la funcion lo va a recibir, * es el operador de desestructuracion,  *kwargs no importa la cantida de parametros nombrados se va a ejecutar
+        func(*args,**kwargs)
         final_time=datetime.now()
         time_elapsed=final_time-initial_time
         print("Pasaron "+str(time_elapsed.total_seconds())+" segundos")
@@ -16,5 +16,13 @@ def execution_time(func):
 def random_func():
     for _ in range (1,100000000):
         pass
+@execution_time
+def suma(a: int,b: int)-> int:
+    return a+b
+@execution_time
+def saludo(nombre="adrian"):
+    print('hola ',nombre)
 
+suma(5,5)
 random_func()
+saludo('Jose')
